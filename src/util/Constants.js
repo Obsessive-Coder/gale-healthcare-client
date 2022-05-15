e761e26c7b4f25ff763fc6608a633c3219ac86d9
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-14 22:25:33 -0500

    add Question 6

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -5,6 +5,12 @@ export default class Constants {
     'The Appendix A section has a series of create table statements.... using these as a guide generate 2 queries.\n1) Write a query where the result is users: user id, username, email, all previous passwords for the user. The query output must list users uniquely.\n2) Write a query that lists polls a user voted in. The results should be username, poll question, total number of votes for the poll.',
 
     'A school has to give all its students an end-of-year examination. The test consists of 40 questions for each student. The question pool is 70 questions. The 40 question test must contain questions 1 - 5 from the question pool for each test. To minimize cheating, the school wants the 40 questions to be randomly ordered. You may not have the same question twice on a test. Code the above in php.',
+
+    'TODO: Do Question 4',
+
+    'TODO: Do Question 5',
+
+    'You have an array of 200 numbers, there are duplicates, write javascript code that would find them and return the number of duplicates in order.',
   ];
 
   static SOLUTIONS = [
@@ -166,7 +172,96 @@ export default class Constants {
         return array_map(fn ($key) => $questions[$key], $randomKeys);
       }
     }
+    `,
 
+    `TODO: Solve question 4`,
+
+    `TODO: Solve question 5`,
+
+    `
+    /**
+     * Running \`node Question_6.js\` will output the results.
+     * Make an array of 200 numbers with duplicates.
+     * Remove the duplicates.
+     * ??? Return the number of duplicates in order.
+     * 
+     * The wording is ambiguous by asking to return the number of duplicates and the duplicates in order.
+     * I would have better understanding if these instructions were attached to a project, and then
+     * ask somebody if it were still too ambiguous to accurately solve.
+     * 
+     * I considered returning both in a JSON like object but thought this to be inefficient
+     * because the client or other consumer of this class can use the length of the
+     * to get the number of duplicates.
+     */
+
+    class Question_6 {
+      solve() {
+        const numbers = this.getNumbers();
+        const duplicateNumbers = this.getDuplicates(numbers);
+
+        // Sort the duplicates in ascending order;
+        duplicateNumbers.sort((a, b) => a - b);
+
+        return duplicateNumbers;
+      }
+
+      getNumbers() {
+        // Make the array of 200 numbers with duplicates.
+
+        let count = 0;
+        let number = Math.floor(Math.random() * 999999);
+        const numbers = [];
+
+        do {
+          numbers.push(number);
+
+          // Get a number 0 or 1. If 1 then reuse the number, else make new number.
+          const isDuplicate = Math.floor(Math.random() * 2) > 0;
+          number = isDuplicate ? number : Math.floor(Math.random() * 999999);
+
+          // Increment count for array length.
+          count++;
+        } while (count < 200);
+
+        // Randomize the numbers since they're built in order and part of the challenge is to sort the duplicates.
+        numbers.sort(() => { return Math.random() - 0.5 });
+
+        return numbers;
+      }
+
+      getDuplicates(numbers = []) {
+        const duplicateNumbers = [];
+
+        for (let i = 0; i < numbers.length; i++) {
+          const firstNumber = numbers[i];
+
+          // Skip this number if its duplicates have already been added.
+          if (duplicateNumbers.includes(firstNumber)) continue;
+
+          for (let j = 0; j < numbers.length; j++) {
+            const secondNumber = numbers[j];
+
+            // Add to the duplicates if they're different indices and the same number.
+            const isDifferentIndex = i !== j;
+            const isSameNumber = firstNumber === secondNumber;
+
+            if (isDifferentIndex && isSameNumber) {
+              duplicateNumbers.push(firstNumber);
+            }
+          }
+
+        }
+
+        return duplicateNumbers;
+      }
+    }
+
+    // Output the results.
+    const question6 = new Question_6();
+    const duplicates = question6.solve();
+
+    console.log('Duplicates in order: ', JSON.stringify(duplicates, null, 2));
+    console.log('Number of duplicates: ', duplicates.length);
     `
   ];
 }
