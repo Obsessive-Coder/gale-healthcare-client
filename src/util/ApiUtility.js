1ddaf72cfd523ad42475257f67c09f3c45f33e6b
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-16 00:08:42 -0500

    fixed cors error?

--- src/util/ApiUtility.js
+++ src/util/ApiUtility.js
@@ -6,11 +6,7 @@ export default class ApiUtility {
   static async solveQuestion(number) {
     try {
       const endpoint = `${API_URL}/question${number}`;
-      const { data } = await axios.get(endpoint, {
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      });
+      const { data } = await axios.get(endpoint);
 
       return data;
     } catch (error) {
