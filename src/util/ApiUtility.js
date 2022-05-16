8debb72dbab84ca654a4e2d459a9a8743a609816
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-15 22:31:13 -0500

    asdfa

--- src/util/ApiUtility.js
+++ src/util/ApiUtility.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://gale-healthcare-backend.herokuapp.com';
 
 export default class ApiUtility {
   static async solveQuestion(number) {
